d6f97f64f0b3af74537a07e03fb3b385e20526ce
starpilgrim2000 <artemchuhlebov@gmail.com>
2023-02-19 17:54:13 +0300

    v.1.0

--- bot.py
+++ bot.py
@@ -1,40 +1,47 @@
-import openai
-import logging
-from aiogram import Bot, Dispatcher, executor, types
-from fastapi import FastAPI
+# Импортировать необходимые библиотеки
 import os
+import openai
+from aiogram import Bot, Dispatcher, executor, types
 
-
+# Загрузить API-токены из переменных окружения
 API_TOKEN = os.environ.get('TG_TOKEN')
 openai.api_key = os.environ.get('OPENAI_TOKEN')
 
-app = FastAPI()
+# Инициализировать бота Telegram и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-logging.basicConfig(level=logging.INFO)
-
+# Определить обработчик сообщения для команды /start
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
+    # Отправить приветственное сообщение пользователю
     await bot.send_message(chat_id=message.chat.id, text="Привет! Я бот с искусственным интеллектом. Чем я могу вам помочь?")
 
+# Определить обработчик сообщения для команды /help
 @dp.message_handler(commands='help')
 async def cmd_help(message: types.Message):
+    # Отправить сообщение, объясняющее назначение бота пользователю
     await bot.send_message(chat_id=message.chat.id, text="Я здесь, чтобы помочь вам с вашими вопросами. Просто напишите свои вопросы, и я сделаю все возможное, чтобы ответить.")
 
+# Определить обработчик сообщения для любых других текстовых сообщений
 @dp.message_handler(content_types=['text'])
 async def handle_text(message: types.Message):
+    # Получить сообщение пользователя в качестве промпта для OpenAI API
     prompt = message.text
+    # Использовать OpenAI API для генерации ответа на сообщение пользователя
     response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
+        engine="text-davinci-003",  # Указать используемый движок (самый мощный и дорогой)
+        prompt=prompt,  # Передать сообщение пользователя в качестве промпта
+        max_tokens=1024,  # Установить максимальную длину сгенерированного ответа
+        n=1,  # Установить количество генерируемых ответов
+        stop=None,  # Установить любые последовательности остановки для ответа
+        temperature=0.5,  # Установить "креативность" ответа
     )
+    # Получить сгенерированный ответ из OpenAI API
     reply = response["choices"][0]["text"]
+    # Отправить ответ пользователю через бота Telegram
     await bot.send_message(chat_id=message.chat.id, text=reply)
 
+# Запустить бота Telegram
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
