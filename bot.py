feb6b9ea9d825ea9b2f4ddf5a731764be6780f15
starpilgrim2000 <artemchuhlebov@gmail.com>
2023-02-19 17:07:03 +0300

    test 4

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await bot.send_message(chat_id=message.chat.id, text="Привет! Я бот с искусственным интеллектом. Чем я могу вам помочь?")
+    await bot.send_message(chat_id=message.chat.id, text="Привет! Я бот1 с искусственным интеллектом. Чем я могу вам помочь?")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
